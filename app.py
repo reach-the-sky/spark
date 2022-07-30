5cad05434396d46c3cd26fe67d010dfeb9354ecc
Krishna Kotni <kotnikrishnachaitanya@gmail.com>
2022-07-30 19:44:19 +0530

    format code structure

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("templates/index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(port=8080)
+    app.run(port=8080,debug=True)
