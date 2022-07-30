bfc61512da64d749f94ab836246fda41c7a42311
Krishna Kotni <kotnikrishnachaitanya@gmail.com>
2022-07-30 20:32:43 +0530

    dockers functions some

--- dockers/functions.py
+++ dockers/functions.py
@@ -2,11 +2,27 @@ import docker
 
 client = docker.from_env()
 
-def list_containers():
+def run(name):
+    client.containers.run(name,detach=True)
+
+def list_images():
     return client.images.list()
 
-def list_container_names():
+def list_containers():
+    return client.containers.list()
+
+def list_images_names():
     result = []
-    for i in list_containers():
+    for i in list_images():
         result.append(i.attrs["RepoTags"][0])
     return result
+
+def list_containers_names():
+    result = []
+    for i in list_containers():
+        result.append(i)
+    return result
+
+def stop_all_containers():
+    for i in list_containers():
+        i.stop()
--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import dockers
 
 if __name__ == "__main__":
-    print("list image container names: ", dockers.list_container_names())
+    dockers.run("ubuntu")
+    print("list image images names: ", dockers.list_images_names())
+    print("list image container names: ", dockers.list_containers_names())
+    dockers.stop_all_containers()
